f30f08334df56d9e9f6dd4d3019c04f11170d0df
AltimorTASDK <altimormc@gmail.com>
2023-09-08 04:09:30 -0400

    formatting

--- code/Game.cs
+++ code/Game.cs
@@ -10,8 +10,8 @@ namespace MyGame;
 
 /// <summary>
 /// This is your game class. This is an entity that is created serverside when
-/// the game starts, and is replicated to the client. 
-/// 
+/// the game starts, and is replicated to the client.
+///
 /// You can use this to create things like HUDs and declare which player class
 /// to use for spawned players.
 /// </summary>
@@ -22,7 +22,7 @@ public partial class MyGame : Sandbox.GameManager
 	/// </summary>
 	public MyGame()
 	{
-		if ( Game.IsClient )
+		if (Game.IsClient)
 		{
 			Game.RootPanel = new Hud();
 		}
@@ -31,24 +31,24 @@ public partial class MyGame : Sandbox.GameManager
 	/// <summary>
 	/// A client has joined the server. Make them a pawn to play with
 	/// </summary>
-	public override void ClientJoined( IClient client )
+	public override void ClientJoined(IClient client)
 	{
-		base.ClientJoined( client );
+		base.ClientJoined(client);
 
 		// Create a pawn for this client to play with
 		var pawn = new Pawn();
 		client.Pawn = pawn;
 		pawn.Respawn();
-		pawn.DressFromClient( client );
+		pawn.DressFromClient(client);
 
 		// Get all of the spawnpoints
 		var spawnpoints = Entity.All.OfType<SpawnPoint>();
 
 		// chose a random one
-		var randomSpawnPoint = spawnpoints.OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
+		var randomSpawnPoint = spawnpoints.OrderBy(x => Guid.NewGuid()).FirstOrDefault();
 
 		// if it exists, place the pawn there
-		if ( randomSpawnPoint != null )
+		if (randomSpawnPoint != null)
 		{
 			var tx = randomSpawnPoint.Transform;
 			tx.Position = tx.Position + Vector3.Up * 50.0f; // raise it up
--- code/pawn/PawnAnimator.cs
+++ code/pawn/PawnAnimator.cs
@@ -7,13 +7,13 @@ public class PawnAnimator : EntityComponent<Pawn>, ISingletonComponent
 {
 	public void Simulate()
 	{
-		var helper = new CitizenAnimationHelper( Entity );
-		helper.WithVelocity( Entity.Velocity );
-		helper.WithLookAt( Entity.EyePosition + Entity.EyeRotation.Forward * 100 );
+		var helper = new CitizenAnimationHelper(Entity);
+		helper.WithVelocity(Entity.Velocity);
+		helper.WithLookAt(Entity.EyePosition + Entity.EyeRotation.Forward * 100);
 		helper.HoldType = CitizenAnimationHelper.HoldTypes.None;
 		helper.IsGrounded = Entity.GroundEntity.IsValid();
 
-		if ( Entity.Controller.HasEvent( "jump" ) )
+		if (Entity.Controller.HasEvent("jump"))
 		{
 			helper.TriggerJump();
 		}
